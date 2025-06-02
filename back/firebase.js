eb57c88aa3048cd1fd9b58e624d15896890ce7cb
BenjaminVC <ben@benvancise.com>
2025-06-02 09:55:22 -0700

    fixed firebase.js

--- back/firebase.js
+++ back/firebase.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfw3CMnjhBdbvGAXTGfXr4vuPChqjpm0I",
-  authDomain: "forkedin-1bfa0.firebaseapp.com",
-  projectId: "forkedin-1bfa0",
-  storageBucket: "forkedin-1bfa0.firebasestorage.app",
-  messagingSenderId: "259671712996",
-  appId: "1:259671712996:web:583fb47091b9d68cad38f1"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
