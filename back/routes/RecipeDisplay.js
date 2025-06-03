011722d22beb1f3f93ab54fbba9952fcba3772de
Jackson <wnj9tx@virginia.edu>
2025-06-03 11:53:47 -0400

    forgor to push the get

---
+++ back/routes/RecipeDisplay.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const router = express.Router();
+
+
+
+//get route 
+
+router.get("/official", async (req, res) => {
+  const { query } = req.query;
+  try {
+    const edamamRes = await axios.get("https://api.edamam.com/api/recipes/v2", {
+      params: {
+        type: "public",
+        q: query,
+        app_id: process.env.EDAMAM_APP_ID,
+        app_key: process.env.EDAMAM_APP_KEY,
+        to: 10
+      }, 
+      headers: {
+        "Edamam-Account-User": "bananavstaco",
+      }
+    });
+    console.log("lol trynna look for this")
+    console.log(edamamRes);
+    res.json(edamamRes.data.hits);
+  } catch (err) {
+    console.error("Failed to fetch Edamam recipes:", err?.response?.data || err.message);
+    res.status(500).json({ error: "my fellow brother of america, we failed to get recipe" });
+  }
+});
+
+
+export default router;
--- front/forkedin/src/routes/RecipeDisplay.jsx
+++ front/forkedin/src/routes/RecipeDisplay.jsx
@@ -1,10 +1,28 @@
 import { useState } from "react"
 import "../styles/RecipeDisplay.css"
+import axios from "axios";
 
 
 
 export default function RecipeDisplay() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [recipes, setRecipes] = useState([]);
 
+    const handleSearch = async () => {
+      if (searchTerm.trim() === "") return;
+
+      console.log("my fellow brother, let's search for this shi");
+      try {
+        console.log(searchTerm)
+        const res = await axios.get(`http://localhost:5001/recipedisplay/official?query=${searchTerm}`)
+        setRecipes(res.data);
+        console.log(res);
+        console.log(res.data);
+      } catch (error) {
+        console.error("bruh food gone gang", error)
+      }
+
+    }
 
 
 
@@ -17,9 +35,15 @@ export default function RecipeDisplay() {
     return (
         <>
         <div className="header-container">
-            <input type="text" className="search-bar" placeholder="Search" />
+             <input
+          type="text"
+          className="search-bar"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
             <button className="button-orange" />
-            <button className="button-green" />
+            <button className="button-green" onClick={handleSearch}/>
         </div>
         <div className="grid-container">
       {Array(8).fill().map((_, i) => (
