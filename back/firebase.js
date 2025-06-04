5b7fb2fea9dfd26c193811a0a1149113ff646385
Jackson <wnj9tx@virginia.edu>
2025-06-04 16:44:01 -0400

    handle search

--- back/firebase.js
+++ back/firebase.js
@@ -1,10 +1,16 @@
 // back/firebase.js
-const admin = require("firebase-admin");
-const serviceAccount = require("./permissions");
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./permissions");
+
+import admin from "firebase-admin";
+// import serviceAccount from "./permissions.json";
+import serviceAccount from "./permissions.json" assert { type: "json" };
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-module.exports = db;
+// module.exports = db;
+export default db;
